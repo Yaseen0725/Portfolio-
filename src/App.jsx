037a9f5025e23fc544cdf56a5caa45590b7f4ef2
Mohd Yaseen <156569712+Yaseen0725@users.noreply.github.com>
2025-02-01 19:01:23 +0530

    Update App.jsx
    Modified navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing Router components
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Service from "./components/Services";
+
 
 function App() {
   return (
@@ -19,10 +22,18 @@ function App() {
                 <div id="home">
                   <Home />
                 </div>
+                 <div id="about">
+                  <About />
+                </div>
+                 <div id="service">
+                  <Services />
+                </div>
               </div>
             }
           />
           <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Services />} /> 
         </Routes>
       </div>
     </Router>
