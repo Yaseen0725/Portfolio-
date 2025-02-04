775650a58b8913540933cffde8e0793729489e61
Mohd Yaseen <156569712+Yaseen0725@users.noreply.github.com>
2025-02-05 00:44:40 +0530

    Update Home.jsx
    Some Modified code in Home Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-
+import { ReactTyped } from "react-typed";
 const Hero = () => {
   return (
     <div className="bg-white text-black text-center py-16">
       <img
         src="src/assets/myImg.jpg"
         alt="Photo"
-        className="mx-auto mb-8 w-48 h-48 rounded-full object-cover transform 
+        className="mx-auto mb-8 w-64 h-64 rounded-full object-cover transform
         transition-transform duration-300 hover:scale-105"
       />
       <h1 className="text-5xl font-bold">
-        I'm{" "}
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 ">
-          Mohd Yaseen
-        </span>
-        , FrontEnd Developer
-      </h1>
+        Hello I'm a{" "}
+        <ReactTyped
+          className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 "
+          strings={["Developer", "Coder", "Student"]}
+          typeSpeed={70}
+          backSpeed={80}
+          loop={true}
+        />
       <p className="mt-4 text-lg text-black text-justify m-8">
-        I am a passionate student with a love for solving challenges through
-        creative solutions. Currently, I’m diving into web development,
-        exploring both front-end and back-end technologies. I enjoy building
-        projects that are functional, user-friendly, and impactful. My goal is
-        to create meaningful solutions that address real-world problems.
+         Welcome to my portfolio! I’m a frontend developer passionate about
+        crafting responsive, interactive, and visually appealing web
+        experiences. Using HTML, CSS, React.js, Tailwind CSS, Bootstrap, and
+        JavaScript, I create seamless and user-friendly interfaces that perform
+        flawlessly across all devices. I focus on delivering clean, optimized
+        code and a smooth user experience. Explore my projects to see how I
+        blend design with functionality to deliver impactful digital experiences
+        that engage and inspire users.
       </p>
       <div className="mt-8 space-x-4">
         <button
@@ -39,11 +44,11 @@ const Hero = () => {
 
         {/* Resume Button */}
         <button
-          onClick={() => window.open("path/to/resume.pdf", "_blank")} // Replace path/to/resume.pdf with your actual resume URL
+          onClick={() => window.open("path/to/resume.pdf", "_blank")}
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-      transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+          transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
-          Resume
+          Download CV
         </button>
       </div>
     </div>
@@ -51,3 +56,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
