079a4e74157950ec869e3f1ec1696fbf7eecca9e
Mohd Yaseen <156569712+Yaseen0725@users.noreply.github.com>
2025-02-06 16:05:17 +0530

    Add files via upload
    Add project Section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-4xl font-bold'>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
